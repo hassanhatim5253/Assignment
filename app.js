e41f1d577122feff58b3363bb1f2e90027d20b6a
hassanhatim5253 <137111036+hassanhatim5253@users.noreply.github.com>
2023-06-28 03:02:20 +0500

    assignment submission

---
+++ app.js
@@ -0,0 +1,79 @@
+const textarea = document.getElementById("textarea1");
+
+function fi1(e) {
+    let value = e.value;
+    textarea.style.fontSize = value + "px";
+}
+
+function fi2(e) {
+    if (textarea.style.fontWeight == "bold") {
+        textarea.style.fontWeight = "normal";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.fontWeight = "bold";
+        e.classList.add("active");
+    }
+}
+
+function fi3(e) {
+    if (textarea.style.fontStyle == "italic") {
+        textarea.style.fontStyle = "normal";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.fontStyle = "italic";
+        e.classList.add("active");
+    }
+}
+
+function fi4(e) {
+    if (textarea.style.textDecoration == "underline") {
+        textarea.style.textDecoration = "none";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.textDecoration = "underline";
+        e.classList.add("active");
+    }
+}
+
+function fi5(e) {
+    textarea.style.textAlign = "left";
+}
+
+function fi6(e) {
+    textarea.style.textAlign = "center";
+}
+
+function fi7(e) {
+    textarea.style.textAlign = "right";
+}
+
+function fi8(e) {
+    if (textarea.style.textTransform == "uppercase") {
+        textarea.style.textTransform = "none";
+        e.classList.remove("active");
+    }
+    else {
+        textarea.style.textTransform = "uppercase";
+        e.classList.add("active");
+    }
+}
+
+function fi9() {
+    textarea.style.fontWeight = "normal";
+    textarea.style.textAlign = "left";
+    textarea.style.fontStyle = "normal";
+    textarea.style.textTransform = "capitalize";
+    textarea.value = "";
+}
+
+function fi10(e) {
+    let value = e.value;
+    textarea.style.color = value;
+}
+
+window.addEventListener('load', () => {
+    textarea.value = "";
+});
